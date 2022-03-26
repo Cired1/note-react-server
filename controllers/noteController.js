74e3ad5225e96515172d242b0dd4b476cdfaa1a1
Cired1 <edabundis@gmail.com>
2022-03-25 21:31:06 -0600

    Add pagination to notes

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -3,10 +3,31 @@ const Note = require("../models/noteModel");
 
 const getNotes = asyncHandler(async (req, res) => {
     const { id } = req.user;
-    const notes = await Note.find({ userId: id });
+
+    let notes = Note.find({ userId: id });
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * pageSize;
+    const total = await Note.countDocuments();
+
+    const pages = Math.ceil(total / pageSize);
+
+    notes = notes.skip(skip).limit(pageSize);
+
+    if (page > pages) {
+        res.status(404);
+        throw new Error("Page not found");
+    }
+
+    const result = await notes;
+
     res.status(200).json({
         success: true,
-        data: notes
+        count: result.length,
+        page,
+        pages,
+        data: result
     });
 })
 
